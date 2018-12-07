4fc0e4d4e88671a5e99175fc1c333d6afed5eece
Jason A. Young <youngj25@southernct.edu>
2018-12-07 01:04:44 -0500

    Added Chart.js
    Fixed the button as well and the html id

--- data.js
+++ data.js
@@ -3,18 +3,42 @@ var socket, Ax = io('/ax', {forceNew:true});
 function setup() {
 	 socket = io.connect('http://localhost:9000');
         
-		 //Stops the Game, it's Over
+	 //Insert Data from the Server
 	 Ax.on('Get Data', function(data){
-		 document.getElementById("salesNumber").innerHTML =" Number of Real Estates Sales Between 2011-2014: "+data.count;
+		 document.getElementById("salesNumber").innerHTML =" Number of Real Estates Sales Between "+document.getElementById("minYear").value+"-"+document.getElementById("maxYear").value+": "+data.count+" ("+Math.floor(data.count*1000/data.total)/10+"%)";
 		 console.log(data);
 	 });
 			
-			
-			
-			
+	
+
+	 var ctx = document.getElementById('myChart').getContext('2d');
+	 var chart = new Chart(ctx, {
+		 // The type of chart we want to create
+		 type: 'line',
+		 // The data for our dataset
+		 data: {
+			 labels: ["January", "February", "March", "April", "May", "June", "July"],
+			 datasets: [{
+				 label: "My First dataset",
+				 backgroundColor: 'rgb(255, 99, 132)',
+				 borderColor: 'rgb(255, 99, 132)',
+				 data: [0, 10, 5, 2, 20, 30, 45],
+			 }]
+		 },
+
+		 // Configuration options go here
+		 options: {}
+	 });
+	 console.log(chart);
+	 //Buttons
 	 document.getElementById("clickMe").addEventListener("click", function(){
 		 console.log("Sent");
-		 Ax.emit('Get Data');
+		 var data = {
+			 minYear : document.getElementById("minYear").value,
+			 maxYear : document.getElementById("maxYear").value
+		 }
+		 console.log(data);
+		 Ax.emit('Get Data',data);
 	 });
 	
 		
